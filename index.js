612de78b6b996a8576bd90a64a732fe305d01000
Alexandra Colevas <Alexandra.Colevas@capitalone.com>
2016-04-09 13:21:13 -0400

    First commit

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'alexandraTestApp',
+  masterKey: process.env.MASTER_KEY || 'alexandraMasterKey123', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'vast-hollows-46150.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
